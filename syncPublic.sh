efada110611ab3716bea7d7bbc45a9672be6cb07
neilwu <wuyupeng2007@gmail.com>
2020-08-31 11:15:00 +0800

    mod

--- syncPublic.sh
+++ syncPublic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rsync --perms -avh --progress --delete ./public/  ../neil-wu.github.io/
+rsync --perms -avh --progress --delete --exclude=".git/*" ./public/  ../neil-wu.github.io/
 
 
 
